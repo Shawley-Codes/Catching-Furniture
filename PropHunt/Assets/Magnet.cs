65f01e168b970b58bf77c1ff610ff40e6f4a7674
MorpheusZx <khaled912@gmail.com>
2021-01-29 21:43:02 -0800

    basic magnet

---
+++ PropHunt/Assets/Magnet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour {
+  public float strength = 10;
+  List<GameObject> reds = new List<GameObject>();
+  List<GameObject> blues = new List<GameObject>();
+  GameObject player;
+  void Start() {
+    player = GameObject.Find("Player");
+    for (int i = 0; i < transform.childCount; ++i) {
+      GameObject obj = transform.GetChild(i).gameObject;
+      if (obj.name.StartsWith("Blue")) blues.Add(obj);
+      if (obj.name.StartsWith("Red")) reds.Add(obj);
+    }
+  }
+
+  void FixedUpdate() {
+    foreach (var obj in reds) obj.GetComponent<Rigidbody>().isKinematic = true;
+    foreach (var obj in blues) obj.GetComponent<Rigidbody>().isKinematic = true;
+
+    List<GameObject> toPull = null;
+    if (Input.GetKey(KeyCode.Alpha1)) { Debug.Log("1"); toPull = reds; }
+    if (Input.GetKey(KeyCode.Alpha2)) toPull = blues;
+
+    if (toPull == null) return;
+    foreach (GameObject obj in toPull) {
+      var rb = obj.GetComponent<Rigidbody>();
+      rb.isKinematic = false;
+      var dir = (player.transform.position - obj.transform.position).normalized;
+      float dist = (player.transform.position - obj.transform.position).magnitude;
+      if (dist < 3) {
+        rb.isKinematic = true;
+      } else {
+        rb.AddForce(dir * strength, ForceMode.Acceleration);
+      }
+    }
+  }
+}
