f2fe3b65e8e8312d60b63e415b76844a4c3a4723
Scott Hawley <iamultima0@gmail.com>
2021-01-30 01:32:40 -0800

    uploaded basic collectable and game mode to track the scene

---
+++ PropHunt/Assets/Collectable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public GameTracker GT;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (other.tag == "Player")
+        {
+            GT.Collected += 1;
+        }
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PropHunt/Assets/GameTracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTracker : MonoBehaviour
+{
+    //obtain list of collectables to get max goals and track progression
+    List<GameObject> collectables = new List<GameObject>();
+    int MaxCollectables = 0;
+    public int Collected = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //yoinked but failed somehow
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            GameObject obj = transform.GetChild(i).gameObject;
+            //i dont know why obj is failing here.
+
+            //if (obj.FindWithTag("Collectable"))) collectables.Add(obj);
+            //if (obj.tag == "Collectable")) collectables.Add(obj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Collected);
+    }
+}
