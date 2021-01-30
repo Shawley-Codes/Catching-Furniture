d2fd944192f554b6599d90f41d533a68698abfe3
MorpheusZx <khaled912@gmail.com>
2021-01-29 20:58:15 -0800

    basic controller

---
+++ PropHunt/Assets/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+  public GameObject player;
+  Vector3 prevPos;
+
+  void Start() {
+    prevPos = player.transform.position;
+  }
+
+  void FixedUpdate() {
+    var curPos = player.transform.position;
+    var delta = curPos - prevPos;
+    transform.position += delta;
+    prevPos = curPos;
+  }
+}
