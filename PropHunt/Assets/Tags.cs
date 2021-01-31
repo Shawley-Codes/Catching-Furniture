57c8f6110a20821621f2b0d12862f71b629ce541
Scott Hawley <iamultima0@gmail.com>
2021-01-30 16:45:04 -0800

    small push to tags

--- PropHunt/Assets/Tags.cs
+++ PropHunt/Assets/Tags.cs
@@ -6,9 +6,16 @@ public class Tags : MonoBehaviour
 {
     //public bool Mangnetizeable = false;
     // enum Color {blue,red,green};
+    bool currentCollectable = false;
     public bool blue = false;
     public bool red = false;
     public bool green = false;
     //public bool Blue = false;
     //public bool Blue = false;
+
+    public void setCurrentCollectable()
+    {
+        currentCollectable = true;
+        gameObject.tag = "Collectable";
+    }
 }
