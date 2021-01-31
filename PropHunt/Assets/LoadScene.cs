f2f9f1043cc1e659a6239b719e6c59c191ba98bb
Scott Hawley <iamultima0@gmail.com>
2021-01-31 09:52:39 -0800

    added main menu and edited build settings

---
+++ PropHunt/Assets/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+    public void loadGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
