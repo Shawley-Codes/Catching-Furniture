a991ab1eb95b16415137f1a46484402568a8c373
Scott Hawley <iamultima0@gmail.com>
2021-01-30 22:10:00 -0800

    update to ui manager and scott level as test

--- PropHunt/Assets/UiManager.cs
+++ PropHunt/Assets/UiManager.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour {
-  public GameObject overlay;
-  public GameObject victoryScreen;
-  public static UiManager instance;
+    public GameObject overlay;
+    public GameObject victoryScreen;
+    public Text count;
+    public Text objective;
+    public static UiManager instance;
 
-  void Awake() {
-    instance = this;
-    overlay.SetActive(true);
-    victoryScreen.SetActive(false);
-  }
+    void Awake() {
+        instance = this;
+        overlay.SetActive(true);
+        victoryScreen.SetActive(false);
+    }
 
-  public void ShowVictory() {
-    victoryScreen.SetActive(true);
-  }
+    public void ShowVictory() {
+        victoryScreen.SetActive(true);
+    }
+
+    public void UpdateCount(int collected){
+        count.text = collected.ToString();
+    }
+
+    public void setObjective(GameObject newItem)
+    {
+        objective.text = newItem.name;
+    }
 }
