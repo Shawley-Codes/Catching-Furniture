aeac8c34432150a7819a81faba0d284d8f8f16ad
Scott Hawley <iamultima0@gmail.com>
2021-01-31 11:08:36 -0800

    addded in pause menu scripts and functions

---
+++ PropHunt/Assets/resumeGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resumeGame : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void resume()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+}
