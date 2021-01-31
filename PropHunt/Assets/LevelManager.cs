92ce822b2b5cf236f0dee746f22a5143c44782c1
MorpheusZx <khaled912@gmail.com>
2021-01-30 18:38:17 -0800

    Loop over magnetizables

--- PropHunt/Assets/LevelManager.cs
+++ PropHunt/Assets/LevelManager.cs
@@ -22,6 +22,8 @@ public class LevelManager : MonoBehaviour {
   List<List<int>> availableTripleColors = new List<List<int>>();
   int nextTripleColor = 0;
   public int Collected = 0;
+  public int numToCollect = 3;
+  HashSet<GameObject> inZone = new HashSet<GameObject>();
 
   void Awake() {
     instance = this;
@@ -31,35 +33,46 @@ public class LevelManager : MonoBehaviour {
   }
 
   //check is object has tag
-  private void OnTriggerEnter(Collider collider)
-  {
-    Debug.Log("Collide", collider.gameObject);
-    //if item is a collectable, increment and set new collectable.
-    if (collider.gameObject.tag == "Collectable")
-    {
-       Collected++;
-       setNewCollectable();
-       //TBD: needs to set ui with collectable count
+  private void OnTriggerEnter(Collider collider) {
+    Debug.Log("Enter", collider.gameObject);
+    if (collider.gameObject.GetComponent<Magnetizable>() != null) {
+      inZone.Add(collider.gameObject);
     }
-    //win if 3 collectables arem collected
-    if (Collected >= 3)
-    {
-       UiManager.instance.ShowVictory();
-    }
+    //Debug.Log("Collide", collider.gameObject);
+    ////if item is a collectable, increment and set new collectable.
+    //if (collider.gameObject.tag == "Collectable")
+    //{
+    //   Collected++;
+    //   setNewCollectable();
+    //   //TBD: needs to set ui with collectable count
+    //}
+    ////win if 3 collectables arem collected
+    //if (Collected >= 3)
+    //{
+    //   UiManager.instance.ShowVictory();
+    //}
   }
 
-
+  //check is object has tag
+  private void OnTriggerExit(Collider collider) {
+    Debug.Log("Exit", collider.gameObject);
+    if (collider.gameObject.GetComponent<Magnetizable>() != null) {
+      inZone.Remove(collider.gameObject);
+    }
+  }
     //called when a new collectable object needs to be set
     //TBD: needs to also set ui with new object
-  public void setNewCollectable()
-  {
-    int maxobj = transform.childCount;
-    int rand = Random.Range(0, maxobj);
-    GameObject obj = transform.GetChild(rand).gameObject;
-    var taggable = obj.GetComponent<Tags>();
-    if(taggable != null) {taggable.setCurrentCollectable();}
-    //recurse until an object can be tagged withg current collectable has been found
-    else { setNewCollectable(); }
+  public void SetCollectibles() {
+    var magnetizables = new List<GameObject>();
+    foreach (var mag in Magnet.instance.gameObject.GetComponentsInChildren<Magnetizable>()) {
+      magnetizables.Add(mag.gameObject);
+    }
+    Debug.Log("Total: " + magnetizables.Count + " magnetizables");
+    Shuffle(magnetizables);
+    for (int i = 0; i < numToCollect && i < magnetizables.Count; ++i) {
+      // var taggable = magnetizables[i].GetComponent<Tags>();
+      // if (taggable != null) { taggable.setCurrentCollectable(); }
+    }
   }
 
   void Shuffle<T>(List<T> v) {
@@ -77,6 +90,8 @@ public class LevelManager : MonoBehaviour {
     currentLevel.SetActive(true);
     if (oldLevel != null) Destroy(oldLevel);
     UiManager.instance?.victoryScreen.SetActive(false);
+    inZone.Clear();
+    SetCollectibles();
     nextSingleColor = 0;
     availableSingleColors.Clear();
     nextDualColor = 0;
